ec3aa69e6498407d09df38056f07908358f1de70
Maxim Kirillov <maximkir181210@gmail.com>
2023-05-25 22:30:22 +0300

    Создал файлы linkedList и deque

---
+++ data_structure/deque.py
@@ -0,0 +1,9 @@
+from data_structure.linkedList import LinkedList
+
+class Deque:
+    # add_first(item) - добавление элемента item в начало
+    # add_last(item) - добавление элемента item в конец
+    # remove_first() - удаляет и возвращает первый элемент
+    # remove_last() - удаляет и возвращает последний элемент
+    # len - возвращает количество элементов
+    pass
